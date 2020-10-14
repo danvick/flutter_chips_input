5643f381306070a98f443ca39e24c4b245a3c504
Dongmin Jang <im@hest.kr>
2020-10-14 22:26:12 +0900

    Fix Bug: Unicode character will disappear when it's complete

--- lib/src/chips_input.dart
+++ lib/src/chips_input.dart
@@ -309,8 +309,6 @@ class ChipsInputState<T> extends State<ChipsInput<T>>
           _enteredTexts.remove(removedChip);
         }
         _updateTextInputState(putText: putText);
-      } else {
-        _updateTextInputState();
       }
       _onSearchChanged(_value.normalCharactersText);
     }
