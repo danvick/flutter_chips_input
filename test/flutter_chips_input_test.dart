6219be33ec831b72ee9ae624ab0c367e9911608e
Anthony Whitford <anthony@whitford.com>
2020-11-04 23:23:55 -0800

    Added draft unit test.

--- test/flutter_chips_input_test.dart
+++ test/flutter_chips_input_test.dart
@@ -1,9 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_chips_input/flutter_chips_input.dart';
+
 void main() {
-  /*test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });*/
+  final allContacts = const [
+    'John Doe',
+    'Jane Doe',
+    'John Smith',
+    'Jane Smith',
+  ];
+
+  testWidgets('ChipsInput', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: ChipsInput<String>(
+            initialValue: allContacts.sublist(1, 3),
+            maxChips: 3,
+            findSuggestions: (String query) => query.isNotEmpty
+                ? allContacts
+                    .where((_) => _.toLowerCase().contains(query.toLowerCase()))
+                    .toList()
+                : const [],
+            onChanged: (contacts) {
+              print(contacts);
+            },
+            chipBuilder: (context, state, contact) {
+              return InputChip(
+                key: ValueKey(contact),
+                label: Text(contact),
+                onDeleted: () => state.deleteChip(contact),
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              );
+            },
+            suggestionBuilder: (context, state, contact) {
+              return ListTile(
+                key: ValueKey(contact),
+                title: Text(contact),
+                onTap: () => state.selectSuggestion(contact),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  });
 }
