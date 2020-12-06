8f6e05bda2fc0093f9bd24c023f3b8777b00b7d3
rettechnologys <75463707+rettechnologys@users.noreply.github.com>
2020-12-06 15:54:18 +0700

    Update chips_input.dart

--- lib/src/chips_input.dart
+++ lib/src/chips_input.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:math';
-
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'suggestions_box_controller.dart';
