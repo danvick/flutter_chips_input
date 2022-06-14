5c15f039b955a33fb27ee6ef8b366496206f0c70
tstrg <silnytomas@gmail.com>
2022-06-14 11:46:10 +0200

    Web - fix entering more than one character
    https://github.com/danvick/flutter_chips_input/issues/34

--- lib/src/chips_input.dart
+++ lib/src/chips_input.dart
@@ -359,7 +359,9 @@ class ChipsInputState<T> extends State<ChipsInput<T>>
             composing: TextRange.empty,
           ));
     }
-    _closeInputConnectionIfNeeded(); //Hack for #34 (https://github.com/danvick/flutter_chips_input/issues/34#issuecomment-684505282). TODO: Find permanent fix
+    if( !kIsWeb) {
+        _closeInputConnectionIfNeeded(); //Hack for #34 (https://github.com/danvick/flutter_chips_input/issues/34#issuecomment-684505282). TODO: Find permanent fix
+    }
     _textInputConnection ??= TextInput.attach(this, textInputConfiguration);
     _textInputConnection?.setEditingState(_value);
   }
